b84d86b954d3cfaa56d855d053c4943de96ca3f5
Cenote <himangshukalita131@gmail.com>
2024-09-28 02:41:51 +0530

    Sign Up UI

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,15 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <img
+        src="https://cdn-icons-png.flaticon.com/128/10631/10631014.png"
+        alt=""
+        width={24}
+        height={24}
+        className="animate-spin"
+      />
+    </div>
+  )
+}
+
+export default Loader
