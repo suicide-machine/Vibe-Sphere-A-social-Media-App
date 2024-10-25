73112040ffbd5785c00c2951b88d66dad53bfd5b
Cenote <himangshukalita131@gmail.com>
2024-10-25 20:44:23 +0530

    Pages and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import Home from "./root/pages/Home"
 import AuthLayout from "./auth/AuthLayout"
 import RootLayout from "./root/RootLayout"
 import { Toaster } from "./components/ui/toaster"
+import Explore from "./root/pages/Explore"
+import Saved from "./root/pages/Saved"
+import AllUsers from "./root/pages/AllUsers"
+import CreatePost from "./root/pages/CreatePost"
+import EditPost from "./root/pages/EditPost"
+import PostDetails from "./root/pages/PostDetails"
+import Profile from "./root/pages/Profile"
+import UpdateProfile from "./root/pages/UpdateProfile"
 
 const App = () => {
   return (
@@ -19,6 +27,14 @@ const App = () => {
         {/* private routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
