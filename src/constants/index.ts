aec7d0d42e9d017ec3916f6fe76ddc29e50e3fbc
Cenote <himangshukalita131@gmail.com>
2024-10-25 20:30:14 +0530

    Left Sidebar

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks = [
+  {
+    imgUrl: "https://cdn-icons-png.flaticon.com/128/2163/2163350.png",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgUrl: "https://cdn-icons-png.flaticon.com/128/2550/2550286.png",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgUrl: "https://cdn-icons-png.flaticon.com/128/1256/1256650.png",
+    route: "/all-users",
+    label: "People",
+  },
+
+  {
+    imgUrl: "https://cdn-icons-png.flaticon.com/128/11462/11462844.png",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgUrl: "https://cdn-icons-png.flaticon.com/128/10747/10747219.png",
+    route: "/create-post",
+    label: "Create Post",
+  },
+]
